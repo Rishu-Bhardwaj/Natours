4a2ab08cb4a506c0c4b5181c0b6a39b4b4a8c00d
Rishu-Bhardwaj <rishubhardwaj2002@gmail.com>
2024-10-05 10:55:19 +0530

    Signup created for Deployment

--- controller/viewsController.js
+++ controller/viewsController.js
@@ -33,6 +33,12 @@ exports.getLoginForm = (req, res) => {
   });
 };
 
+exports.getSignupForm = (req, res) => {
+  res.status(200).render('signup', {
+    title: 'Create your Account',
+  });
+};
+
 exports.getAccount = (req, res) => {
   res.status(200).render('account', {
     title: 'Your Account',
--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,13 @@
 /* eslint-disable */
 // import '@babel/polyfill';
 import { login, logout } from './login.js';
+import { signup } from './signup.js';
 import { updateSettings } from './updateSettings.js';
 import { bookTour } from './stripe.js';
 
 // DOM ELEMENTS
 const loginForm = document.querySelector('.form--login');
+const signupForm = document.querySelector('.form--signup');
 const logOutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
 const userPasswordForm = document.querySelector('.form-user-password');
@@ -20,6 +22,16 @@ if (loginForm)
   });
 
 if (logOutBtn) logOutBtn.addEventListener('click', logout);
+
+if (signupForm)
+  signupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('passwordConfirm').value;
+    signup(name, email, password, passwordConfirm);
+  });
 
 if (userDataForm)
   userDataForm.addEventListener('submit', (e) => {
